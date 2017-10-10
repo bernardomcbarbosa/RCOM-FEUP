2459b2b2e2134feb18d865e4a58b1ab198296371
Bernardo Barbosa <up201503477@fe.up.pt>
2017-10-10 17:15:42 +0100

    begin tp3

---
+++ tp3/sender.c
@@ -0,0 +1,162 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+volatile int STOP=FALSE;
+
+char set[5] = {0x7E,0x03,0x03,0x00,0x7E};
+char UA[5] = {0x7E,0x03,0x07,0x04,0x7E};
+int mode;
+int fd,fi,size;
+
+int llopen(char* port){
+  struct termios oldtio,newtio;
+
+  fd = open(port, O_RDWR | O_NOCTTY );
+    if (fd <0) {
+		perror(port);
+		exit(-1);
+	}
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
+
+
+
+  /*
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+    leitura do(s) pr�ximo(s) caracter(es)
+  */
+
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+  return 0;
+}
+
+void llclose(){
+  close(fd);
+}
+
+void llwrite(char* msg){
+    int res,i;
+    char buff[12000];
+
+    res = read(fi,buff,size);
+    for(i=0;i<size;i++){
+      res = write(fd,buff+i,i);
+      printf("%d\n",i);
+    }
+	// for(i=0;i<5;i++)
+  //   	res = write(fd,msg+i,1);
+    // sleep(3);
+  }
+
+void llread(){
+  int i,res=0;
+  char buffer[5],conf;
+
+  //S1
+  i=0;
+  while (res==0) {
+    res = read(fd,buffer,1);
+    }
+i++;
+//S2
+while ((res!=0) && (buffer[0] == 0x7E)) {
+    res = read(fd,buffer+i,1);
+	if(buffer[i] != 0x7E)
+		break;
+    }
+//S3
+i++;
+while ((res!=0) && (buffer[i] != 0x7E)) {
+    res = read(fd,buffer+i,1);
+	i++;
+    }
+
+//READER MSG
+	for(i=0;i<5;i++)
+		printf("0x%x |",buffer[i]);
+	printf("\n");
+
+	if(!mode){
+		conf = buffer[1]^buffer[2];
+		if(conf == buffer[3])
+	  		llwrite(UA);
+		else
+			printf("Message Invalid\n");
+	}
+	else{
+		conf = buffer[1]^buffer[2];
+		if(conf == buffer[3])
+			printf("Sucess\n");
+		else
+			printf("Message Invalid\n");
+	}
+}
+
+int main(int argc, char** argv)
+{
+	char buf[5];
+	int res,i;
+  struct stat st;
+
+    if ( (argc < 3) ||
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1 pinguim.gif\n");
+      exit(1);
+    }
+
+    if (llopen(argv[1]) == -1){
+      printf("Can't open %s\n",argv[1]);
+      exit(1);
+    }
+
+	fi = open(argv[2],O_RDONLY);
+	// fseek(fi, 0, SEEK_END); // seek to end of file
+	// size = ftell(fi); // get current file pointer
+	// fseek(f, 0, SEEK_SET); // seek back to beginning of file
+  fstat(fi, &st);
+  size = st.st_size;
+
+  //printf("%i\n",size);
+	mode = 1;
+
+        llwrite(set);
+		    //   llread();
+    llclose();
+    return 0;
+  }
