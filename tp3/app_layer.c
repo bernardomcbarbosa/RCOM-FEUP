8960985c16b93dd8caae3b4254772b9d942c3265
diogomotapinto <diogomotapinto@gmail.com>
2017-10-17 16:03:30 +0100

    testas tamanho do ficheiro

--- tp3/app_layer.c
+++ tp3/app_layer.c
@@ -27,6 +27,8 @@ int connection( const char *port, int status){
 void send_file(const char *port,char* filename){
   int fi,size,f;
   struct stat st;
+  char *fileSize;
+  int resto = 256;
 
   fi = open(port,O_RDONLY);
   if(fi < 0){
@@ -40,12 +42,26 @@ void send_file(const char *port,char* filename){
   size = st.st_size;
 
   //START PACKET
-  char* start_packet = (char *)malloc(size);
+  char* start_packet = (char *)malloc(3;
   start_packet[0] = START_C2;
   start_packet[1] = FILE_SIZE;
-  start_packet[2] = size;
+  start_packet[2] = 0x04;
 
+  fileSize =  (char *)malloc(size);
+
+for (size_t i = 0; i < 4; i++) {
+    fileSize[i] = size % resto;
+    resto = resto % 256;
+}
   printf("%d\n",size);
+
+start_packet = (char *) realloc(start_packet, sizeof(start_packet) + sizeof(fileSize)+ );
+
+strcat(start_packet, fileSize);
+
+start_packet[7]= FILE_NAME;
+
+start_packet[8] = strlen(filename);
 
 
 
