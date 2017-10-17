2bd3074d7ef664a046eb076baca68586c7927b73
diogomotapinto <diogomotapinto@gmail.com>
2017-10-17 15:11:47 +0100

    testas tamanho do ficheiro

--- tp3/compile.sh
+++ tp3/compile.sh
@@ -1,4 +1,4 @@
 gcc -Wall app_layer.c -c
 gcc -Wall data_layer.c -c
 gcc -Wall main.c -o main data_layer.o app_layer.o
-scp -r /root/Desktop/RCOM-FEUP/tp3 netedu@192.168.109.23:/home/netedu/Desktop
+#scp -r /root/Desktop/RCOM-FEUP/tp3 netedu@192.168.109.23:/home/netedu/Desktop
