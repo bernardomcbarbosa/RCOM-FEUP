f7978a139b71c3ba62b95f1bc1d2ded40377b303
João Sá <up201506252@fe.up.pt>
2018-01-05 13:33:00 +0000

    Organizing 1st project

--- project_1/Bernardo.sh
+++
@@ -1,3 +0,0 @@
-#scp -r /root/Desktop/tp3 netedu@192.168.109.25:/home/netedu/Desktop
-scp data_layer.c data_layer.h app_layer.c app_layer.h main.c rcom@192.168.1.251:/home/rcom/Desktop/tp3
-scp data_layer.c data_layer.h app_layer.c app_layer.h main.c rcom@192.168.1.252:/home/rcom/Desktop/tp3
--- project_1/Sa.sh
+++
@@ -1,2 +0,0 @@
-scp -r ~/git/RCOM-FEUP jotapsa@192.168.1.7:/home/jotapsa/git
-scp -r ~/git/RCOM-FEUP jotapsa@192.168.1.8:/home/jotapsa/git
